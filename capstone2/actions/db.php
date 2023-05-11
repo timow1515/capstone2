266539916cdc688e3dfbbec601f6e62e0f889713
timow1515 <130204633+timow1515@users.noreply.github.com>
2023-05-11 21:19:51 +0800

    Add files via upload

---
+++ capstone2/actions/db.php
@@ -0,0 +1,11 @@
+<?php
+// Connect to database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "new_db";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
